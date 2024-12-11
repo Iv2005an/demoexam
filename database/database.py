ec6afb902816a4ca1a1950fc334bf50522bb39b8
Ivan Telitsin <telitsinivan05dev@yandex.ru>
2024-12-11 21:54:30 +0300

    feat: создание таблиц БД

--- database/database.py
+++ database/database.py
@@ -5,3 +5,8 @@ from database.config import get_database_url
 
 engine = create_engine(get_database_url(), echo=True)
 SessionFactory = sessionmaker(bind=engine)
+
+
+def init_db():
+    from database.models import Base
+    Base.metadata.create_all(engine)
